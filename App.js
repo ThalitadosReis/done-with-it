1ebd4ba3b80b7879344b06a24b3cf50f3ec39dfd
Thalita dos Reis <dosreistha@gmail.com>
2023-11-07 15:47:18 +0100

    input components

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  return <MessagesScreen />;
 }
