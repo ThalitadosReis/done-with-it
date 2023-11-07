82dcbdd245b7f5d3956535020ca6dca25cfb5308
Thalita dos Reis <dosreistha@gmail.com>
2023-11-07 15:56:17 +0100

    forms exercises: register screen, listing edit screen, placeholder color and organising lists components

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import React from "react";
-
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />;
 }
